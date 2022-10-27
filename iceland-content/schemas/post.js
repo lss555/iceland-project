7c3ed221e384d789e71e48bc11678b4306439dc3
lss555 <chrisheibel7@gmail.com>
2022-10-27 11:31:51 -0600

    initial commit

---
+++ iceland-content/schemas/post.js
@@ -0,0 +1,51 @@
+export default {
+  name: 'content',
+  title: 'Content',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      description: 'Just for CMS use to declare what is being edited',
+      type: 'string',
+    },
+    {
+      name: 'header',
+      title: 'Header',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    },
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const {author} = selection
+      return Object.assign({}, selection)
+    },
+  },
+}
