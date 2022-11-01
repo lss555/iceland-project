dadc9874f3ab1c38133610a8fe7412c8bccee32c
lss555 <chrisheibel7@gmail.com>
2022-11-01 13:22:50 -0600

    Gallery cards

--- src/components/gallery/gallery-styles.js
+++ src/components/gallery/gallery-styles.js
@@ -12,7 +12,6 @@ export const GalleryHeader = styled.h1`
 	height: 179px;
 	left: 120px;
 	top: 1280px;
-
 	font-family: 'Clash Display';
 	font-style: normal;
 	font-weight: 400;
@@ -21,12 +20,17 @@ export const GalleryHeader = styled.h1`
 	text-align: justify;
 	letter-spacing: -0.03em;
 	text-transform: uppercase;
-
 	color: #EEEEEE;
+	@media screen and (max-width: 2000px) {
+    	left: 1rem;
+  }
 `;
 
 export const GalleryTextContainer = styled.div`
 	
+	@media screen and (max-width: 2000px) {
+    	left: 1rem;
+  }
 `;
 
 export const GallerySmallTextOne = styled.div`
@@ -45,6 +49,9 @@ export const GallerySmallTextOne = styled.div`
 	letter-spacing: -0.03em;
 	text-indent: 30px;
 	color: #EEEEEE;
+	@media screen and (max-width: 2000px) {
+    	left: 1rem;
+  }
 `;
 
 export const GallerySmallTextTwo = styled.div`
@@ -63,6 +70,9 @@ export const GallerySmallTextTwo = styled.div`
 	letter-spacing: -0.03em;
 	text-indent: 30px;
 	color: #EEEEEE;
+	@media screen and (max-width: 2000px) {
+    	left: 1rem;
+  }
 `;
 
 export const GallerySmallTextThree = styled.div`
@@ -82,8 +92,111 @@ export const GallerySmallTextThree = styled.div`
 	text-indent: 30px;
 
 	color: #EEEEEE;
+	@media screen and (max-width: 2000px) {
+    	left: 20rem;
+  }
 `;
 
 export const GallerySmallTextContainer = styled.div`
 
+`;
+
+export const GalleryCardsContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	margin-left: 60rem;
+	width: 100%;
+	height: 800px;
+	@media screen and (max-width: 2000px) {
+    	margin-left: auto;
+    	width: 50%;
+    	margin-right: -5rem;
+  }
+`;
+
+export const GalleryCard = styled.img`
+	display: flex;
+
+	box-sizing: border-box;
+	left: 60rem;
+	width: 530px;
+	height: 800px;
+	top: 200px;
+	border: 0.795918px solid #151515;
+	filter: drop-shadow(10px 10px 25px rgba(0, 0, 0, 0.25));
+	border-radius: 5px;
+	object-fit: cover;
+	@media screen and (max-width: 2000px) {
+    	width: 400px;
+    	
+  }
+`;
+
+export const IndividualCardCotnainer = styled.div`
+	display: flex;
+	position: relative;
+	height: 800px;
+	z-index: 3;
+	top: 200px;
+	justify-content: flex-start;
+	align-items: flex-end;
+	text-align: flex-end;
+	:hover {
+		z-index: 4; 
+		transition: all .7s ease;
+		transform: scale(1.1) rotate(2deg);
+	}
+`;
+
+export const IndividualCardCotnainer2 = styled.div`
+	display: flex;
+	justify-content: center;
+	position: relative;
+	height: 800px;
+	margin-left: ;
+	z-index: 2;
+	top: 200px;
+	margin-left: -322px;
+	justify-content: flex-start;
+	align-items: flex-end;
+	text-align: flex-end;
+	:hover {
+		z-index: 4; 
+		transition: all .7s ease;
+		transform: scale(1.1) rotate(2deg);
+`;
+
+export const IndividualCardCotnainer3 = styled.div`
+	display: flex;
+	position: relative;
+	height: 800px;
+	top: 200px;
+	margin-left: -322px;
+	z-index: 1;
+	width: 530px;
+	justify-content: flex-start;
+	align-items: flex-end;
+	text-align: flex-end;
+	:hover {
+		z-index: 4; 
+		transition: all .7s ease;
+		transform: scale(1.1) rotate(2deg);
+	}
+`;
+
+export const GalleryCardText = styled.h2`
+	display: flex;
+	position: absolute;
+	text-align: flex-end;
+	margin-right: 0;
+	align-items: flex-end;
+	margin-left: 3rem;
+	font-weight: 400;
+	font-size: 26.2737px;
+	line-height: 32px;
+	color: #EEEEEE;
+	letter-spacing: -0.03em;
+	text-transform: uppercase;
+	font-family: 'Clash Display';
+	font-style: normal;
 `;
--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import sanityClient from "../../client.js";
-import { GalleryContainer, GalleryHeader, GalleryTextContainer, GallerySmallTextOne, GallerySmallTextTwo, GallerySmallTextThree, GallerySmallTextContainer } from './gallery-styles.js';
+import { GalleryContainer, GalleryHeader, GalleryTextContainer, GallerySmallTextOne, GallerySmallTextTwo, GallerySmallTextThree, GallerySmallTextContainer, GalleryCardsContainer, GalleryCard, IndividualCardCotnainer, IndividualCardCotnainer2, IndividualCardCotnainer3, GalleryCardText } from './gallery-styles.js';
 import BlockContent from "@sanity/block-content-to-react";
+import Image1 from '../assets/rob-nelson.jpg';
+import Image2 from '../assets/jakob.jpg';
+import Image3 from '../assets/iceland-waterfall.jpg';
 
 const Gallery = () => {
 	const [galleryContent, setGalleryContent] = useState(null);
@@ -44,6 +47,20 @@ const Gallery = () => {
 					</GallerySmallTextThree>
 				</GallerySmallTextContainer>
 			</GalleryTextContainer>
+			<GalleryCardsContainer>
+				<IndividualCardCotnainer>
+					<GalleryCard src={Image1}></GalleryCard>
+					<GalleryCardText>Skógafoss, Iceland</GalleryCardText>
+				</IndividualCardCotnainer>
+				<IndividualCardCotnainer2>
+					<GalleryCard src={Image2}></GalleryCard>
+					<GalleryCardText>Vik, Iceland</GalleryCardText>
+				</IndividualCardCotnainer2>
+				<IndividualCardCotnainer3>
+					<GalleryCard src={Image3}></GalleryCard>
+					<GalleryCardText>Gullfoss Falls, Iceland</GalleryCardText>
+				</IndividualCardCotnainer3>
+			</GalleryCardsContainer>
 		</GalleryContainer>
 		)
 };
